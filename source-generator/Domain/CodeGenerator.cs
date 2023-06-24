da3f8c46206dfa3c1dccf3b76bfc735c1fe03d56
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-06-24 18:44:40 +0300

    rename & add Microsoft.CodeAnalysis

---
+++ source-generator/Domain/CodeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Domain;
+
+[Generator]
+public class CodeGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        // Code generation goes here
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // No initialization required for this one
+    }
+}
