76bda7e91d00629989caf1d0b7ad8a033b001c3a
Sefer Mirza <sefermirza@gmail.com>
2023-07-05 18:12:11 +0300

    add application model

---
+++ source-generator/Domain/ApplicationModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+public class ApplicationModel
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public object[] Properties { get; set; }
+    public object[] StaticProperties { get; set; }
+    public Operation Operations { get; set; }
+}
+
+public class Operation
+{
+    public string OperationName { get; set; }
+    public object ReturnValue { get; set; }
+}
