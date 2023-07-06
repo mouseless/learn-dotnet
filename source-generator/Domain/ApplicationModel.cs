e70d166e5cd6500c87f90bd560e84663126477a6
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-07-06 15:09:23 +0300

    update application model & schema.json

--- source-generator/Domain/ApplicationModel.cs
+++ source-generator/Domain/ApplicationModel.cs
@@ -4,13 +4,40 @@ public class ApplicationModel
 {
     public string Id { get; set; }
     public string Name { get; set; }
-    public object[] Properties { get; set; }
-    public object[] StaticProperties { get; set; }
-    public Operation Operations { get; set; }
+    public Operation[] Operations { get; set; }
 }
 
 public class Operation
 {
-    public string OperationName { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
     public object ReturnValue { get; set; }
+}
+
+public static class ApplicationModelExtensions
+{
+    public static string ControllerTemplate(this ApplicationModel source) =>
+$@"// Auto-generated code
+using Microsoft.AspNetCore.Mvc;
+
+namespace {source.Id};
+
+[ApiController]
+[Route("""")]
+public class {source.Name} : ControllerBase
+{{
+    public string Get()
+    {{
+        return ""This is {source.Name}"";
+    }}
+    {string.Join("", source.Operations.Select(operation =>
+$@"
+    [HttpGet(""/{operation.Name}"")]
+    public {operation.Type} {operation.Name}()
+    {{
+        return ""{operation.ReturnValue}"";
+    }}"
+        )
+     )}
+}}";
 }
