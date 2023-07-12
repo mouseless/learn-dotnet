f38e2eb3f5b64a0d657b3489c265d61528d35b74
Sefer Mirza <sefermirza@gmail.com>
2023-07-12 17:47:31 +0300

    get namespace data to target project

--- source-generator/CodeGen/CodeToJsonSchemaGenerator.cs
+++ source-generator/CodeGen/CodeToJsonSchemaGenerator.cs
@@ -1,29 +1,45 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using Newtonsoft.Json;
+using System.Collections.Immutable;
 
 namespace CodeGen;
 
 [Generator(LanguageNames.CSharp)]
 public class CodeToJsonSchemaGenerator : IIncrementalGenerator
 {
+    class AnalyzerConfig
+    {
+        public string ControllerServicesNamespace { get; set; }
+    }
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         var compilationProvider = context.CompilationProvider;
+        var additionalTextsProvider = context.AdditionalTextsProvider.Where(at => at.Path.EndsWith("analyzer.config.json")).Select((text, _) => text.GetText());
 
-        context.RegisterSourceOutput(compilationProvider, (spc, compilation) =>
-            Execute(spc, compilation));
+        var combine = compilationProvider.Combine(additionalTextsProvider.Collect());
+
+        context.RegisterSourceOutput(combine, (spc, compilation) =>
+        {
+            var config = compilation.Right.FirstOrDefault();
+            var analyzerConfig = config != null ? Deserialize<AnalyzerConfig>(config.ToString()) : null;
+            var @namespace = analyzerConfig != null ? analyzerConfig.ControllerServicesNamespace : "";
+
+            Execute(spc, compilation.Left, @namespace);
+        });
     }
 
-    private void Execute(SourceProductionContext spc, Compilation compilation)
+    private void Execute(SourceProductionContext spc, Compilation compilation, string texts)
     {
-        var models = GetApplicationModel(compilation);
+        var models = GetApplicationModel(compilation, texts);
 
         foreach (var model in models)
         {
             string appModel = Serialize(model);
 
-            spc.AddSource("generated.cs", $@"
+            spc.AddSource($"{model.Name}.generated.cs", $@"
 namespace X;
 
 static class Y
@@ -40,13 +56,15 @@ static class Y
 
     private string Serialize(ApplicationModel source) => JsonConvert.SerializeObject(source);
 
-    private List<ApplicationModel> GetApplicationModel(Compilation context)
+    private T Deserialize<T>(string source) => JsonConvert.DeserializeObject<T>(source);
+
+    private List<ApplicationModel> GetApplicationModel(Compilation compilation, string @namespace)
     {
         var result = new List<ApplicationModel>();
 
-        foreach (var tree in context.SyntaxTrees)
+        foreach (var tree in compilation.SyntaxTrees)
         {
-            var semanticModel = context.GetSemanticModel(tree);
+            var semanticModel = compilation.GetSemanticModel(tree);
 
             var classDeclarations = tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>();
 
@@ -54,8 +72,7 @@ static class Y
             {
                 var classSymbol = semanticModel.GetDeclaredSymbol(classDeclaration) as INamedTypeSymbol;
 
-                // ToDo - WebApp.System bilgisi csprojdan alınacak
-                if (classSymbol?.ContainingNamespace?.ToString() == "WebApp.System" &&
+                if (classSymbol?.ContainingNamespace?.ToString() == @namespace &&
                     classSymbol?.DeclaredAccessibility == Accessibility.Public)
                 {
                     ApplicationModel applicationModel = new();
