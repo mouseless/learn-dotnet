a33e9580eadd4524ead03de38a9e06a0e2189cdd
Sefer Mirza <sefermirza@gmail.com>
2023-07-13 16:36:28 +0300

    add workflow for build

--- source-generator/CodeGen/CodeToJsonSchemaGenerator.cs
+++ source-generator/CodeGen/CodeToJsonSchemaGenerator.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
 using Newtonsoft.Json;
-using System.Collections.Immutable;
 
 namespace CodeGen;
 
