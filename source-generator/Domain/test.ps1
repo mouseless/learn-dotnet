bf10f841d5d2f08b82f4a24433afdb7056851589
Sefer Mirza <sefermirza@gmail.com>
2023-07-13 16:54:36 +0300

    review

--- source-generator/Domain/test.ps1
+++ source-generator/Domain/test.ps1
@@ -1,8 +1,6 @@
 param($bin, $config, $netversion)
 
 $cli = "$PSScriptRoot\..\Cli\bin\$config\$netversion\Cli.exe"
-$schema = "$PSScriptRoot"
-$output = "$PSScriptRoot"
 
 & $cli '../Domain/Generated/CodeGen/CodeGen.CodeToJsonSchemaGenerator/Scheduler.generated.cs' "../Domain/bin/$config/$netversion/Scheduler.schema.json" "../WebApp/bin/$config/$netversion/Scheduler.schema.json"
 
