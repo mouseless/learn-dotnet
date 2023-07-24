70b25acaa5667509e4aa1bf76ce6c449d317d537
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-07-24 17:57:41 +0300

    add test base and new test

--- unit-testing/UnitTesting/FluentAssertions.cs
+++ unit-testing/UnitTesting/FluentAssertions.cs
@@ -2,7 +2,7 @@
 
 namespace UnitTesting;
 
-public class FluentAssertions
+public class FluentAssertions : Testbase
 {
     [Test]
     public void When_text_is_clipped__Remove_the_spaces_before_and_after()
@@ -11,5 +11,20 @@ public class FluentAssertions
         string expected = "Test Text";
 
         actual.Trim().Should().Be(expected);
+    }
+
+    [Test]
+    public void Triple_A_pattern()
+    {
+        // arrange
+        Person person = APerson(name: "Mark");
+
+        // act
+        _context.Add(person);
+        _context.Save();
+
+        // assert
+        _context.Persons.Should().NotBeEmpty();
+        // daha eklenecek
     }
 }
---
+++ unit-testing/UnitTesting/Testbase.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTesting;
+
+public class Testbase
+{
+    protected Repository _context;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _context = new Repository();
+    }
+
+    protected Person APerson(string name = "") => new Person();
+}
