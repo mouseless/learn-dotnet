b837c5d1885b0d44141dc72d1dcc79f269f31d90
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-07-26 12:50:25 +0300

    update action & add shouldly test

--- unit-testing/README.md
+++ unit-testing/README.md
@@ -1,1 +1,9 @@
 # Unit Testing
+
+## 3A Pattern
+
+## Shouldly Library
+
+## Fluent Assertion Library
+
+## GiveMe & MockMe Usage
--- unit-testing/UnitTesting/ShouldlyTests.cs
+++ unit-testing/UnitTesting/ShouldlyTests.cs
@@ -12,4 +12,17 @@ public class ShouldlyTests
 
         actual.Trim().ShouldBe(expected);
     }
+
+    [Test]
+    public void When_forge_is_Given_length_and_raw_Than_a_sword_is_created()
+    {
+        // arrange
+        var Forge = GiveMe.AForge();
+
+        // act
+        Forge.CreateASword(length: 10, raw: "Iron");
+
+        // assert
+        Forge.CreatedSwords.ShouldContainKeyAndValue(10, "Iron");
+    }
 }
