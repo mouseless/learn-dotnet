0e7224152d9ab893efa896ad5c4dfc92569ff277
Sefer Mirza <sefermirza@gmail.com>
2023-08-28 16:56:29 +0300

    add ruleset and configure rule files

--- analyzers/Stylecop.Analyzers/Test.cs
+++ analyzers/Stylecop.Analyzers/Test.cs
@@ -1,6 +1,17 @@
-﻿namespace StylecopAnalyzers;
+﻿namespace Stylecop.Analyzers;
 
 public class Test
 {
+    private readonly string name;
 
+    public int MyProperty { get; set; }
+
+    public Test()
+    {
+    }
+
+    private string TestMethod()
+    {
+        return "Test";
+    }
 }
