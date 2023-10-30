5c652b5794c08df64903d4371594fd5a24c149f9
alptuncs <alptuncsever@gmail.com>
2023-10-30 18:20:26 +0300

    test commit

--- source-generator/Domain/postBuild.sh
+++ source-generator/Domain/postBuild.sh
@@ -7,5 +7,9 @@ netversion=$3
 generatepath=$4
 
 cli=$basedir/../Cli/bin/$config/$netversion/Cli
+pdb=$basedir/../Cli/bin/$config/$netversion/Cli.pdb
+dll=$basedir/../Cli/bin/$config/$netversion/Cli.dll
 
+eval $pdb domain ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.generated.cs ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.schema.json
+eval $dll domain ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.generated.cs ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.schema.json
 eval $cli domain ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.generated.cs ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.schema.json
