1446872b4ed7698e811e1a121de64ea3401132e9
Sefer Mirza <sefermirza@gmail.com>
2023-11-01 17:12:37 +0300

    change eol

--- source-generator/Domain/postBuild.sh
+++ source-generator/Domain/postBuild.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
 basedir=$(dirname "$0")
-bin=$1
-config=$2
-netversion=$3
-generatepath=$4
+bin="$1"
+config="$2"
+netversion="$3"
+generatepath="$4"
+tr -d "\r" < file
+cli="$basedir/../Cli/bin/$config/$netversion/Cli"
 
-cli=$basedir/../Cli/bin/$config/$netversion/Cli
-
-eval $cli domain ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.generated.cs ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.schema.json
+eval "$cli" domain ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.generated.cs ../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.schema.json
