3c2d5e6b4a72a3b3a4123a7999e2d9315f71997a
Sefer Mirza <sefermirza@gmail.com>
2023-11-02 17:48:14 +0300

    add new project for regex source generators

---
+++ regex-source-generators/RegexSourceGenerators/LearnRegex.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexSourceGenerators;
+
+public partial class LearnRegex
+{
+    [GeneratedRegex("abc|def", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex RegexOnlyLetter();
+
+    public static void GetMeLetters(string text)
+    {
+        if (RegexOnlyLetter().IsMatch(text))
+        {
+            // Take action with matching text
+        }
+    }
+}
