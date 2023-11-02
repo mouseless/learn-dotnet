b0a426e6a24d6788c55d521e9137f9cc22918bdd
Sefer Mirza <sefermirza@gmail.com>
2023-11-02 11:48:49 +0300

    review changes

--- source-generator/Domain/postBuild.sh
+++ source-generator/Domain/postBuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 basedir=$(dirname "$0")
 bin="$1"
 config="$2"
@@ -6,5 +7,5 @@ netversion="$3"
 generatepath="$4"
 
 cli="$basedir/../Cli/bin/$config/$netversion/Cli"
-echo "cli path: $cli"
+
 eval "$cli" "domain" "../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.generated.cs" "../Domain/$generatepath/CodeGen/CodeGen.JsonSchemaGenerator/Domain.schema.json"
