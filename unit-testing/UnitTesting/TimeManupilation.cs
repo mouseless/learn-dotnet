83d8af36aed6345d7d1ba8cd33e4030ecbc715e1
Sefer Mirza <sefermirza@gmail.com>
2023-12-15 15:01:51 +0300

    add faketimeprovider using example

---
+++ unit-testing/UnitTesting/TimeManupilation.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Time.Testing;
+
+namespace UnitTesting;
+
+public class TimeManupilation : Spec
+{
+    class Information(TimeProvider _timeProvider)
+    {
+        public bool SendMails() =>
+            _timeProvider.GetLocalNow().Day == 8;
+    }
+
+    [Test]
+    public void Mails_are_sent_on_the_10th()
+    {
+        var fakeTime = new FakeTimeProvider();
+        fakeTime.SetUtcNow(new DateTimeOffset(2025, 12, 8, 23, 59, 59, TimeSpan.Zero));
+        var days = new Information(fakeTime);
+
+        days.SendMails().ShouldBeTrue();
+    }
+}
