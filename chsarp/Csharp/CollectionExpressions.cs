21cd6348b9e866d5232bee4c78e87c16f14d75d1
alptuncs <alptuncsever@gmail.com>
2023-12-15 12:10:48 +0300

    add collection expression cheat sheet

---
+++ chsarp/Csharp/CollectionExpressions.cs
@@ -0,0 +1,21 @@
+﻿namespace Csharp;
+
+public class CollectionExpressions
+{
+    public void EmptyCollectionInitialization()
+    {
+        int[] intArray = [];
+        List<int> intList = [];
+        Dictionary<string, int> dictionary = [];
+    }
+
+    public void CollectionInitialization()
+    {
+        int[] intArray = [1, 2, 3, 4, 5];
+        List<int> intList = [1, 2, 3, 4, 5];
+        Dictionary<string, int> dictionary = new() { ["test"] = 1, ["test2"] = 2 };
+
+        int[] intCollection = [.. intArray, .. intList];
+        int[] anotherIntCollection = [.. intCollection, 1, 2, 3, 4, 5];
+    }
+}
