2123f92247de8937f186aa04f6547f59069d5959
Sefer Mirza <sefermirza@gmail.com>
2023-12-15 15:53:37 +0300

    update test

--- unit-testing/UnitTesting/TimeManupilation.cs
+++ unit-testing/UnitTesting/TimeManupilation.cs
@@ -11,7 +11,7 @@ public class TimeManupilation : Spec
     }
 
     [Test]
-    public void Mails_are_sent_on_the_10th()
+    public void Mails_are_sent_on_the_8th()
     {
         var fakeTime = new FakeTimeProvider();
         fakeTime.SetUtcNow(new DateTimeOffset(2025, 12, 8, 23, 59, 59, TimeSpan.Zero));
