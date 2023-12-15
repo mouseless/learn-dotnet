c3e83de7ca87f844209d5ad87b5439a3accd14e5
alptuncs <alptuncsever@gmail.com>
2023-12-15 15:01:09 +0300

    log example usage

--- chsarp/Csharp/CollectionExpressions.cs
+++ chsarp/Csharp/CollectionExpressions.cs
@@ -1,24 +1,37 @@
-﻿namespace Csharp;
+﻿using Microsoft.Extensions.Logging;
 
-public class CollectionExpressions
+namespace Csharp;
+
+public class CollectionExpressions(ILogger<CollectionExpressions> _logger)
 {
     public void EmptyCollectionInitialization()
     {
         int[] intArray = [];
+        _logger.LogInformation($"Empty Array initialization: {string.Join(',', intArray)}");
+
         List<int> intList = [];
+        _logger.LogInformation($"Empty List initialization: {string.Join(',', intList)}");
+
         Dictionary<string, int> dictionary = [];
+        _logger.LogInformation($"Empty Dictionary initialization: {string.Join(',', dictionary.Keys)} {string.Join(',', dictionary.Values)}");
     }
 
     public void CollectionInitialization()
     {
         int[] intArray = [1, 2, 3, 4, 5];
-        List<int> intList = [1, 2, 3, 4, 5];
-        Dictionary<string, string> dictionary = new() { ["key"] = "value" };
+        _logger.LogInformation($"Array initialization: {string.Join(',', intArray)}");
+
+        List<int> intList = [6, 7, 8, 9, 10];
+        _logger.LogInformation($"List initialization: {string.Join(',', intList)}");
+
+        Dictionary<string, string> dictionary = new() { ["key"] = "someValue" };
+        _logger.LogInformation($"Dictionary initialization: {string.Join(',', dictionary.Keys)} {string.Join(',', dictionary.Values)}");
 
         int[] intCollection = [.. intArray, .. intList];
-        int[] anotherIntCollection = [.. intCollection, 1, 2, 3, 4, 5];
+        _logger.LogInformation($"Array initialization with list and array spread elements: {string.Join(',', intCollection)}");
 
-        Console.WriteLine(anotherIntCollection);
+        int[] anotherIntCollection = [.. intCollection, 1, 2, 3, 4, 5];
+        _logger.LogInformation($"Array initialization with spread element and members: {string.Join(',', anotherIntCollection)}");
     }
 
     public void CallMethods()
@@ -28,6 +41,13 @@ public class CollectionExpressions
         SomeMethod(args: ["arg1", "arg2", "arg3"]);
     }
 
-    public void SomeMethod(params string[] args) { }
-    public void SomeOtherMethod(string[] args) { }
+    public void SomeMethod(params string[] args)
+    {
+        _logger.LogInformation($"SomeMethod is called with args: {string.Join(',', args)}");
+    }
+
+    public void SomeOtherMethod(string[] args)
+    {
+        _logger.LogInformation($"SomeOtherMethod is called with args: {string.Join(',', args)}");
+    }
 }
--- chsarp/Csharp/Program.cs
+++ chsarp/Csharp/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Csharp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddSingleton<CollectionExpressions>();
+
+serviceCollection.AddLogging(options =>
+{
+    options.AddConsole();
+    options.SetMinimumLevel(LogLevel.Debug);
+});
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var collectionExpressions = serviceProvider.GetRequiredService<CollectionExpressions>();
+
+collectionExpressions.EmptyCollectionInitialization();
+collectionExpressions.CollectionInitialization();
+collectionExpressions.CallMethods();
