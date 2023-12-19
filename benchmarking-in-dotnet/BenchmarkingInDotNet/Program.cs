0ae8b47c5c81e3040878d37881cc7b635941f6d3
dncsvr <disever10@alm.ku.edu.tr>
2023-12-19 11:40:48 +0300

    add sample test class for demo
    - update .gitignore

--- benchmarking-in-dotnet/BenchmarkingInDotNet/Program.cs
+++ benchmarking-in-dotnet/BenchmarkingInDotNet/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using BenchmarkingInDotNet;
+
 Console.WriteLine("Hello, World!");
+
+BenchmarkRunner.Run<Testing>();
---
+++ benchmarking-in-dotnet/BenchmarkingInDotNet/Testing.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace BenchmarkingInDotNet;
+
+[SimpleJob(launchCount: 1, warmupCount: 10, iterationCount: 20)]
+public class Testing
+{
+    [Benchmark]
+    public string TestString()
+    {
+        var result = string.Empty;
+
+        for (int i = 0; i < 100; i++)
+        {
+            result += i;
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public string TestStringBuilder()
+    {
+        var result = new StringBuilder();
+
+        for (int i = 0; i < 100; i++)
+        {
+            result.Append(i);
+        }
+
+        return result.ToString();
+    }
+}
