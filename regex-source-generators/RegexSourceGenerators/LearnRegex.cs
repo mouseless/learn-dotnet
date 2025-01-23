643e67c934a3d4f16e7b999f7f34b82dc4181f6a
Sefer Mirza <sefermirza@gmail.com>
2025-01-23 19:17:16 +0300

    add property example to learn regex

--- regex-source-generators/RegexSourceGenerators/LearnRegex.cs
+++ regex-source-generators/RegexSourceGenerators/LearnRegex.cs
@@ -5,7 +5,15 @@ namespace RegexSourceGenerators;
 public static partial class LearnRegex
 {
     [GeneratedRegex(@"^(https?|ftp)://[^\\s/$.?#].[^\\s]*$", RegexOptions.IgnoreCase, "en-US")]
-    private static partial Regex RegexIsUrl();
+    private static partial Regex RegexIsUrl { get; }
 
-    public static bool IsUrl(this string url) => RegexIsUrl().IsMatch(url);
+    [GeneratedRegex(@"^(?:https?://)?(?:www\.)?([^:/\?#]+)", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex RegexGetDomain();
+
+    public static bool IsUrl(this string url) => RegexIsUrl.IsMatch(url);
+    public static string GetDomain(this string url)
+    {
+        var match = RegexGetDomain().Match(url);
+        return match.Success ? match.Groups[1].Value : string.Empty;
+    }
 }
