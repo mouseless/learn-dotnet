056fd2e82a3ee3aff0cdc0be1b90caf6c3b4789f
Sefer Mirza <sefermirza@gmail.com>
2025-05-10 14:53:14 +0300

    init 'issue/localization'

---
+++ localization/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
